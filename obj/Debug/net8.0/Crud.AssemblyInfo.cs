063e402c6a5b1157b93cfe127bdbdead6cfb97ec
Juanky <juancamiloherrera960@gmail.com>
2024-03-21 01:44:50 -0500

    Favicon cada pag, funciones UsuariosController, Show Usuarios

--- obj/Debug/net8.0/Crud.AssemblyInfo.cs
+++ obj/Debug/net8.0/Crud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Crud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5ef7a6b0056bf0a88c24639909e3de319f65e59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f34e6324366a840f5d09b7011c849725310f4ebf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Crud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Crud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
